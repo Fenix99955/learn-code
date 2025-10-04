f32bbaac02b9067242377729623c5ecca31ed198
nova.devmain <salinagormez@gmail.com>
2025-10-04 14:42:05 +0530

    jsjtan

---
+++ jsjs.js
@@ -0,0 +1,284 @@
+//console.log("")
+//console.log("")
+//
+//window.alert("")
+//window.alert("")
+//
+//this is a comment
+
+/*
+this is also a comment
+*/
+
+
+//textify brouser using ids
+document.getElementById("kk").textContent="MELLO"
+document.getElementById("nk").textContent="CHELLO"
+document.getElementById("wk").textContent="JELLO"
+
+//variables
+/*
+let x=123
+
+console.log(x)
+
+let age = 25
+console.log(age)
+
+let price=10
+console.log(price)
+
+let grade=2.1
+console.log(grade)
+console.log("you are an idiot")
+console.log(`you are ${age} years of age`)
+console.log(typeof grade)
+
+
+
+console.log(typeof "hugsdkjhfk")
+console.log(typeof 4654964)
+*/
+
+/*
+//booleans
+
+let x= true
+
+console.log(`bro is online: ${x}`)
+
+let y= false
+
+console.log(`bro is online: ${y}`)
+
+
+let z= "asd"
+
+console.log(typeof(z)=='string')
+
+*/
+
+//variables but in the window
+/*
+let x ="jam"
+let y ="froot"
+let z ="juice"
+
+document.getElementById("p1").textContent=x
+document.getElementById("p2").textContent=y
+document.getElementById("p3").textContent=z
+*/
+
+//arethicmatic operator
+/*
+let std= 30
+
+console.log(std+1)
+console.log(std**5)
+console.log(std-12)
+console.log(std%2)
+console.log(std*15)
+console.log(std/5)
+*/
+
+//input window
+/*
+let a;
+a=window.prompt("what is your name")
+
+alert(a)
+
+let b
+
+document.getElementById("submit").onclick=function(){
+    b=document.getElementById("input").value
+    document.getElementById("lll").textContent=b
+}
+*/
+
+
+
+//typeconversion
+/*
+let age = window.prompt("how old are you")
+age=Number(age)+1
+alert(age,typeof age)
+*/
+
+//math
+
+/*
+Math.PI
+
+console.log(Math.PI)
+
+
+let x= 3.21
+let y = 2
+let z
+z=Math.round(x)
+let t=Math.floor(x)
+console.log(t)
+console.log(z)
+console.log(Math.ceil(x))
+console.log(Math.pow(5,2))
+console.log(Math.sqrt(y))
+console.log(Math.log(10,100))
+console.log(Math.sin(45))
+console.log(Math.max(12,15,55))
+console.log(Math.max(12,15,55))
+*/
+
+
+
+
+/*
+//random number generator
+
+let x;
+
+x=Math.random()*(6 - 1)+1
+console.log(Math.floor(x))
+
+*/
+
+/*
+//if else
+let x=window.prompt("how big your willy 1 big or 2 no?")
+
+if (x =="big"){
+    alert("you gettin laid")
+}
+
+else{
+    alert("virgin forever")
+}
+
+*/
+
+//checkbox programing in a different folder
+
+//terniary operator "?"
+/*
+let x= 25
+
+let mes = x>=18 ? "major": "minor"
+console.log(mes)
+*/
+
+
+/*
+//switch effeicient way to replace if/else
+let day = "pissa"
+ switch(day){
+    case 1:
+        console.log("monday")
+        bresk
+
+    case 2 :
+        console.log("tuesday")
+        break
+    default:
+        console.log("gay")
+
+ }
+*/
+
+//string method
+
+/*
+
+let a ="gaya  sfuuk"
+console.log(a[0])
+
+console.log(a.indexOf("g"))
+
+console.log(a.trim())
+
+console.log(a.repeat(12))
+
+*/
+
+
+//string slicing
+
+/*
+let a="asd usb"
+
+
+console.log(a.slice(0,4))
+console.log(a.slice(5,8))
+
+*/
+/*
+  //chaining in js
+
+
+//no method chaining
+
+let x= window.prompt("jello migga")
+
+
+//method chaining
+
+
+
+
+
+*/
+/*
+
+//logical operator
+
+
+
+
+
+*/
+//strict equality   "==="
+/*
+let a = 555
+
+if  (a ===555){
+    console.log("yes")
+}
+
+*/  
+
+/*
+let i 
+
+for (i=0;i<7;i=i+3){
+    console.log(i)
+
+}
+*/
+
+
+
+//functions
+
+
+
+//variable scope local/global
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
